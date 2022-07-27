581293db218b8169d7f2c15584a1a5184be4db85
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-27 21:33:25 +0530

    How to solve a dsa problem added

--- Questions with answers/Array/One.cpp
+++ Questions with answers/Array/One.cpp
@@ -1,16 +1,5 @@
 // Question = Given an array of integers, return the indices of the two numbers that add up to a given target.
 
-// How to approch a problem
-
-//1.  Verify the constraints.(ask question about the question)
-//2. Write out some test cases. 
-//3. Figure out a solution without a code.
-//4. Write out our solution in code.
-//5. Double check for errors.
-//6. Test the code with all the test cases.
-//7. Space and time complexity.
-//8. Optimize solution.
-
 #include<bits/stdc++.h>
 using namespace std;
 
---
+++ Questions with answers/How_To_Solve_A_Question.md
@@ -0,0 +1,10 @@
+// How to approch a problem
+
+//1.  Verify the constraints.(ask question about the question)
+//2. Write out some test cases. 
+//3. Figure out a solution without a code.
+//4. Write out our solution in code.
+//5. Double check for errors.
+//6. Test the code with all the test cases.
+//7. Space and time complexity.
+//8. Optimize solution.
