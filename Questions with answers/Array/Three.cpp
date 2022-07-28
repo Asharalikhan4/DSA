7d05b3a983d8ab14dee139f2c23d446f1e984c25
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-28 11:02:40 +0530

    Three added

---
+++ Questions with answers/Array/Three.cpp
@@ -0,0 +1,7 @@
+// Question: Given an array of integers representing an elevation map where the width of each bar is 1, return how much rainwater can be trapped.
+
+#include<bits/stdc++.h>
+
+int main(){
+    return 0;
+}
