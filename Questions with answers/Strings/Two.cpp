6b927fd379b727aea8085cd8bc53ba4780255443
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-28 19:36:21 +0530

    New file added

---
+++ Questions with answers/Strings/Two.cpp
@@ -0,0 +1,22 @@
+//Question: Given a string, find the length of the longest substring without repeating characters.
+
+/*
+Subsequence - abcbbd
+SubString - abcbbd
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSubString(string s){
+    if(s.length() <= 1) return s.length;
+    int longest = INT_MIN;
+    for(int left = 0; left < s.length(); left++){
+        
+    }
+}
+
+int main(){
+    string s = "abccabb";
+    return 0;
+}
